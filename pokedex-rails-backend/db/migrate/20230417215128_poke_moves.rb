67bd83bab98d90cc3d6ed010f599f1d9c86aec19
David M Gudeman <davidmgudeman@gmail.com>
2023-04-17 15:03:56 -0700

    setup schema

---
+++ pokedex-rails-backend/db/migrate/20230417215128_poke_moves.rb
@@ -0,0 +1,12 @@
+class PokeMoves < ActiveRecord::Migration[7.0]
+  def change
+    create_table :poke_moves do |t|
+      t.references :pokemon, null: false, foreign_key: true, index: false
+      t.references :move, null: false, foreign_key: true
+      t.index [:pokemon_id, :move_id], unique: true
+
+      t.timestamps
+
+    end
+  end
+end
---
+++ pokedex-rails-backend/db/migrate/20230417220115_add_indexto_moves.rb
@@ -0,0 +1,5 @@
+class AddIndextoMoves < ActiveRecord::Migration[7.0]
+  def change
+    add_index :moves, :name, unique: true
+  end
+end
